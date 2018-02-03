2d208a35cfea5ba5f99019cbb1a86b85708bab94
nekomeo <linh.tu545@gmail.com>
2018-02-03 13:58:43 -0800

    Initial repo & backend setup. Add .gitignore file

---
+++ testmailchimp/.virtualenvs/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/elleti/Projects/Django/Django-Mailchimp/testmailchimp/.virtualenvs/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
